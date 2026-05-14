4c56db347cba6ff8038e7a48f8bdfd83f84af185
Cyprian Shagba <donaldcyprian12@gmail.com>
2026-05-14 15:49:35 +0100

    Serve favicon directly

--- pudpredict/urls.py
+++ pudpredict/urls.py
@@ -2,14 +2,22 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView
+from django.http import FileResponse
 from django.urls import include, path
-from django.views.generic.base import RedirectView
+from pathlib import Path
 
 from patients.forms import VerifiedAuthenticationForm
 
 
+def favicon(request):
+    icon_path = Path(settings.BASE_DIR) / "frontend" / "static" / "assets" / "pudpredict-favicon.svg"
+    response = FileResponse(open(icon_path, "rb"), content_type="image/svg+xml")
+    response["Cache-Control"] = "public, max-age=86400"
+    return response
+
+
 urlpatterns = [
-    path("favicon.ico", RedirectView.as_view(url="/static/assets/pudpredict-favicon.svg?v=8", permanent=True)),
+    path("favicon.ico", favicon, name="favicon"),
     path("accounts/login/", LoginView.as_view(authentication_form=VerifiedAuthenticationForm, template_name="registration/login.html"), name="login"),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
